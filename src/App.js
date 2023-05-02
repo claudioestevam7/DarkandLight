28f355ac5e68d7d38b8205070035f809711d85d4
claudioestevam7 <claudioestevam7@hotmail.com.br>
2023-05-02 02:38:28 -0300

    commit #2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import "./App.css";
-import styled from "styled-components";
-import Hero from "./components/Hero";
-import { Contexto } from "./components/Contexto";
-import { useState } from "react";
+import React, { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './styles/theme'
+import GlobalStyles from './styles/globalStyles'
 
-function App() {
-
-  const [dark, setDark] = useState(true)
+function App (){
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   return (
-    <Contexto.Provider value={{dark, setDark}}>
-        <Hero />
-    </Contexto.Provider>
-  );
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <div className="container">
+        <GlobalStyles />
+        <h1>Dark Theme</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <button className="accent" 
+          onClick={() => setIsDarkTheme(!isDarkTheme)}>
+          Trocar Tema
+        </button>
+      </div >
+    </ThemeProvider>
+  )
 }
 
 export default App;
